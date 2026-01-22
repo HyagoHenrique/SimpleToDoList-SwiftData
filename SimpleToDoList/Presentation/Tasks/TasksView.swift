05b396bcfcf309a23c9451feaf126e6952ec4270
Hyago Henrique <hyagoh@icloud.com>
2026-01-22 00:54:09 -0300

    TasksView, ViewModel, fix Interactor and App file.

---
+++ SimpleToDoList/Presentation/Tasks/TasksView.swift
@@ -0,0 +1,7 @@
+//
+//  TasksView.swift
+//  SimpleToDoList
+//
+//  Created by Hyago Henrique on 22/01/26.
+//
+
---
+++ SimpleToDoList/Presentation/TasksList/TasksListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TasksListViewModel.swift
+//  SimpleToDoList
+//
+//  Created by Hyago Henrique on 22/01/26.
+//
+
+import Foundation
+import Observation
+
+@Observable
+final class TasksListViewModel {
+    private let interactor: TaskIntera
+}
